b011e950b7d55860ec2ba136b9b486af28e0c221
Sawan <itissaavn@gmail.com>
2023-11-29 18:17:07 +0530

    add basic routing

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      Home
+    </main>
+  );
+};
+
+export default Home;
