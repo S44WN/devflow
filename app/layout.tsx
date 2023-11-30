d46fb4e8c2e0f56a3b4d0829208e923b5a56e452
Sawan <itissaavn@gmail.com>
2023-12-01 01:46:19 +0530

    add navbar(basic) + shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const spaceGrotesk = Space_Grotesk({
   variable: "--font-spaceGrotesk",
 });
 
-export const meta: Metadata = {
+export const metadata: Metadata = {
   title: "DevFlow",
   description:
     "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
